8894c56fa5116ea5fd3bf82702151654bb6fac52
Saihaj Toor <saihaj.toor@student.kpu.ca>
2026-01-31 12:26:59 -0800

    Initial commit

---
+++ src/main/java/com/saihaj/sfustudbuds/SfustudbudsApplication.java
@@ -0,0 +1,13 @@
+package com.saihaj.sfustudbuds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfustudbudsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfustudbudsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saihaj/sfustudbuds/SfustudbudsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saihaj.sfustudbuds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfustudbudsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
